91faeb5401bf02c298e9c71bc9e90e6ff629360e
Zerolaer <111960461+Zerolaer@users.noreply.github.com>
2025-09-28 16:57:02 +0400

    фвы

--- dist/assets/format-CfBjB5Ts.js
+++
@@ -1,1 +0,0 @@
-const n=new Intl.NumberFormat("ru-RU",{style:"currency",currency:"EUR",maximumFractionDigits:2});function t(r){return r==null||isNaN(Number(r))?"€0.00":n.format(Number(r))}export{t as f};
